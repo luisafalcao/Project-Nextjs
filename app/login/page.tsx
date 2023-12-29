bda64ad14986d50f0bf78e892471139402796183
luisafalcao <luisafalcaovaz@gmail.com>
2023-12-29 18:59:26 -0300

    Revert "Auth"
    This reverts commit b3047cf9fc77135debd24f5cac3eaf10e0a7e319.

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -2,7 +2,7 @@ import LoginForm from "@/app/lib/ui/login-form";
 
 export default function LoginPage() {
     return (
-        <main className="flex items-center justify-center h-fit">
+        <main className="flex items-center justify-center">
             <div className="relative mx-auto flex w-full flex-col space-y-2.5 p-4">
                 <LoginForm />
             </div>
