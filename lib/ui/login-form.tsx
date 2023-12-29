094a0bd2b760d75716dac33cb57a096e97d34964
luisafalcao <luisafalcaovaz@gmail.com>
2023-12-29 12:04:48 -0300

    test deployment

--- lib/ui/login-form.tsx
+++ lib/ui/login-form.tsx
@@ -12,26 +12,45 @@ export default function LoginForm() {
                 <h1 className="mb-3 text-2xl">Entrar no Sistema</h1>
                 <div className="w-full">
                     <div>
-                        <label className="mb-3 mt-5 block text-xs font-medium text-gray-900" htmlFor="email">
+                        <label 
+                            className="mb-3 mt-5 block text-xs font-medium text-gray-900" 
+                            htmlFor="email"
+                        >
                             Email
                         </label>
                         <div className="relative">
-                            <input className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
-                                id="email" type="email" name="email" required />
+                            <input 
+                                className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
+                                id="email" 
+                                type="email" 
+                                name="email" 
+                                required 
+                            />
                         </div>
                     </div>
                     <div className="mt-4">
-                        <label className="mb-3 mt-5 block text-xs font-medium text-gray-900" htmlFor="senha">
+                        <label 
+                            className="mb-3 mt-5 block text-xs font-medium text-gray-900" 
+                            htmlFor="senha"
+                        >
                             Password
                         </label>
                         <div className="relative">
-                            <input className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
-                                id="senha" type="password" name="senha" required minLength={6} />
+                            <input 
+                                className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
+                                id="senha" 
+                                type="password" 
+                                name="senha" 
+                                required 
+                                minLength={6} 
+                            />
                         </div>
                     </div>
                 </div>
                 <div className="container py-5 px-5 mx-0 min-w-full flex flex-col items-center">
-                    <button className="m-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Logar</button>
+                    <button className="m-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                        Logar
+                    </button>
                 </div>
                 <div className="flex h-5 items-end space-x-1">
                     {errorMessage && (
