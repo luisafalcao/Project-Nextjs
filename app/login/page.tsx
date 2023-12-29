b3047cf9fc77135debd24f5cac3eaf10e0a7e319
luisafalcao <luisafalcaovaz@gmail.com>
2023-12-29 18:47:06 -0300

    Auth

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -2,7 +2,7 @@ import LoginForm from "@/app/lib/ui/login-form";
 
 export default function LoginPage() {
     return (
-        <main className="flex items-center justify-center">
+        <main className="flex items-center justify-center h-fit">
             <div className="relative mx-auto flex w-full flex-col space-y-2.5 p-4">
                 <LoginForm />
             </div>
